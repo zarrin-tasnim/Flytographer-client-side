d64d658df03fd57945ccf0acf0367d1e04ac941d
zarrin-tasnim <zarrintasnim200420@gmail.com>
2022-11-10 21:16:53 +0600

    social login google added

--- src/Pages/Shared/SocialLogin/SocialLogin.js
+++ src/Pages/Shared/SocialLogin/SocialLogin.js
@@ -1,12 +1,32 @@
 import React, {useContext} from 'react';
 import { AuthContext } from '../../../contexts/AuthProvider/AuthProvider';
+import { setAuthToken } from '../../../Api/auth';
+import { useLocation, useNavigate } from "react-router-dom";
+
+
 
 const SocialLogin = () => {
     const { googleSignIn } = useContext(AuthContext);
+    const location = useLocation();
+    const navigate = useNavigate();
+
+    const from = location.state?.from.pathname || '/';
+
+    const handelGoogleSignIn = () => {
+        googleSignIn()
+            .then(result => {
+                const user = result.user;
+                console.log(user)
+                setAuthToken(user);
+                navigate(from, { replace: true });
+            })
+            .catch(error => console.log(error))
+    }
+
     return (
         <div>
             <p className='text-center'>
-                <button className='btn btn-ghost bg-black'>Google</button>
+                <button onClick={handelGoogleSignIn} className='btn btn-ghost'> Google</button>
             </p>
         </div>
     );
